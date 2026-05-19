399d3ca100ce6a535c95db2a0ba30981182b016e
pznoriegaj <jpznoriega17@gmail.com>
2026-05-18 23:12:19 -0600

    commit de prueba

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,23 @@
 const botonGuardar = document.getElementById('btn-guardar');
 const entradaTexto = document.getElementById('entrada-texto');
+const historial = document.getElementById('historial');
 botonGuardar.addEventListener('click', () => {
     const texto = entradaTexto.value;
     console.log(texto);
-    localStorage.setItem('entradas', texto);
+    const listaEntradas = JSON.parse(localStorage.getItem('entradas')) || [];
+    listaEntradas.push(texto);
+    localStorage.setItem('entradas', JSON.stringify(listaEntradas));
+    const parrafo = document.createElement('p'); //Crea un parrafo vacio 
+    parrafo.textContent = texto; // agrega el texto del usuario 
+    historial.appendChild(parrafo);
 });
+
+function cargarEntradas() {
+    const entradas = JSON.parse(localStorage.getItem('entradas')) || [];
+    entradas.forEach(function(entrada) {
+        const parrafo = document.createElement('p');
+        parrafo.textContent = entrada;
+        historial.appendChild(parrafo);
+    });
+}
+cargarEntradas();
