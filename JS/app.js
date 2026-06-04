50104eb5940bed2e9090b695cf5f5d72942e9d00
pznoriegaj <jpznoriega17@gmail.com>
2026-06-03 20:49:27 -0600

    Se agrega en el diario, el marcaje de fechas y el historial mas detallado con el seguimiento por dias, meses y años, se modifica el color de las fuentes otra vez, el idioma a elegir para el diario.

--- JS/app.js
+++ JS/app.js
@@ -3,13 +3,17 @@ const entradaTexto = document.getElementById('entrada-texto');
 const historial = document.getElementById('historial');
 botonGuardar.addEventListener('click', () => {
     const texto = entradaTexto.value;
+    const fecha = new Date();
     console.log(texto);
     const listaEntradas = JSON.parse(localStorage.getItem('entradas')) || [];
-    listaEntradas.push(texto);
+    listaEntradas.push({ texto: texto, fecha: fecha });/* se agrega un objeto con el texto y la fecha a la lista de entradas */
     localStorage.setItem('entradas', JSON.stringify(listaEntradas));
     const parrafo = document.createElement('p'); //Crea un parrafo vacio 
     parrafo.textContent = texto; // agrega el texto del usuario 
     historial.appendChild(parrafo);
+    const fechaElemento = document.createElement('small'); // agrega la fecha de entradad del usuario
+    fechaElemento.textContent = fecha.toLocaleString("es-MX", {weekday: "long", year: "numeric", month: "long", day: "numeric"}); // formatea la fecha en formato legible
+    historial.appendChild(fechaElemento);
      entradaTexto.value = ''; /* se agrega este string vacio para limpiar la entrada despues de guarar la entrada */
 });
 
@@ -17,8 +21,13 @@ function cargarEntradas() {
     const entradas = JSON.parse(localStorage.getItem('entradas')) || [];
     entradas.forEach(function(entrada) {
         const parrafo = document.createElement('p');
-        parrafo.textContent = entrada;
+        parrafo.textContent = entrada.texto;
+        const fechaElemento = document.createElement('small');
+        const fecha = new Date(entrada.fecha);
+        fechaElemento.textContent = fecha.toLocaleString("es-MX", {weekday: "long", year: "numeric", month: "long", day: "numeric"});
         historial.appendChild(parrafo);
+        historial.appendChild(fechaElemento);
     });
 }
 cargarEntradas();
+
