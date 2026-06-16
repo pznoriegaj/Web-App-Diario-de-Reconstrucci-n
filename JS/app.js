9f362d330cb3adfc516713cefd83b62bea4c0915
pznoriegaj <jpznoriega17@gmail.com>
2026-06-15 20:01:14 -0600

    se agregan los botones para eliminar entradas y asi mismo limpíar el historia

--- JS/app.js
+++ JS/app.js
@@ -15,18 +15,28 @@ botonGuardar.addEventListener('click', () => {
     fechaElemento.textContent = fecha.toLocaleString("es-MX", {weekday: "long", year: "numeric", month: "long", day: "numeric"}); // formatea la fecha en formato legible
     historial.appendChild(fechaElemento);
      entradaTexto.value = ''; /* se agrega este string vacio para limpiar la entrada despues de guarar la entrada */
+
 });
 
 function cargarEntradas() {
     const entradas = JSON.parse(localStorage.getItem('entradas')) || [];
-    entradas.forEach(function(entrada) {
+    entradas.forEach(function(entrada, indice) {
         const parrafo = document.createElement('p');
         parrafo.textContent = entrada.texto;
         const fechaElemento = document.createElement('small');
         const fecha = new Date(entrada.fecha);
         fechaElemento.textContent = fecha.toLocaleString("es-MX", {weekday: "long", year: "numeric", month: "long", day: "numeric"});
+        const botonEliminar = document.createElement('button'); // se crea un boton para eliminar las entradas del historial
+        botonEliminar.textContent = 'Eliminar';
+        botonEliminar.addEventListener('click',() => { // se agrega un evento click al boton eliminar
+            entradas.splice(indice, 1); // se elimina la entrada del arreglo de entradas
+            localStorage.setItem('entradas', JSON.stringify(entradas)); // se actualiza el local storage con la nueva lista de entradas
+            historial.innerHTML = ''; // se limpia el historial para volver a cargar las entradas actualizadas
+            cargarEntradas(); // se vuelve a cargar las entradas para reflejar los cambios
+        });
         historial.appendChild(parrafo);
         historial.appendChild(fechaElemento);
+        historial.appendChild(botonEliminar);// se agrega boton al historial para eliminar la entrada
     });
 }
 cargarEntradas();
