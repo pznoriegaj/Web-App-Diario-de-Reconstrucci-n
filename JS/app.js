d553a69eabc4f485a38ef244d870e23260f490ed
pznoriegaj <jpznoriega17@gmail.com>
2026-06-17 20:49:45 -0600

    se agrega un evento click el cual servira para mandar la indicacion de guardardo con la tecla ENTER, se agrega el atajo con el comando SHFIT+ENTER, para los saltos de linea , asi mismo se borran las lineas 11 a 16, y se agrega historial.innerHTMLm, para poder tener el boton eliminar en la nuevas etradas a crear proximamente, se espera el agregado o modificacion de los botones eliminar en la proxima leccion

--- JS/app.js
+++ JS/app.js
@@ -8,14 +8,15 @@ botonGuardar.addEventListener('click', () => {
     const listaEntradas = JSON.parse(localStorage.getItem('entradas')) || [];
     listaEntradas.push({ texto: texto, fecha: fecha });/* se agrega un objeto con el texto y la fecha a la lista de entradas */
     localStorage.setItem('entradas', JSON.stringify(listaEntradas));
-    const parrafo = document.createElement('p'); //Crea un parrafo vacio 
-    parrafo.textContent = texto; // agrega el texto del usuario 
-    historial.appendChild(parrafo);
-    const fechaElemento = document.createElement('small'); // agrega la fecha de entradad del usuario
-    fechaElemento.textContent = fecha.toLocaleString("es-MX", {weekday: "long", year: "numeric", month: "long", day: "numeric"}); // formatea la fecha en formato legible
-    historial.appendChild(fechaElemento);
      entradaTexto.value = ''; /* se agrega este string vacio para limpiar la entrada despues de guarar la entrada */
-
+});
+  entradaTexto.addEventListener('keydown', (evento) => {
+    if (evento.key === 'Enter' && !evento.shiftKey) { // se agrega un evento para que al presionar enter se guarde la entrada
+        evento.preventDefault(); // se previene el comportamiento por defecto de la tecla enter
+        botonGuardar.click(); // se simula un click en el boton guardar para guardar la entrada
+        historial.innerHTML = ''; // se limpia el historial para volver a cargar las entradas actualizadas
+        cargarEntradas(); // se vuelve a cargar las entradas para reflejar los cambios
+    }
 });
 
 function cargarEntradas() {
